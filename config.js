9bfeaff85428ed54e2b05b593914f725fc9052e1
Itsmeji <141827510+Itsmeji@users.noreply.github.com>
2023-11-30 00:54:56 +0000

    Update config.js

--- config.js
+++ config.js
@@ -33,7 +33,7 @@ global.SupportGroupLink = "https://chat.whatsapp.com/GyuNhffyyuc3PVq6JMZyG8";
 
 
 //
-global.sessionName = "creds-1.json";                          //Do not change.
+global.sessionName = "A17-session";                          //Do not change.
 
 
 //
